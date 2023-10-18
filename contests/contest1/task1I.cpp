405ae8a559cf9bae900f39256e0413ed09c7e1c0
kaif777 <73842611+kaif777@users.noreply.github.com>
2023-10-18 22:53:48 +0300

    contest 1 task I

--- contests/contest1/task1I.cpp
+++ contests/contest1/task1I.cpp
@@ -1,6 +1,70 @@
 #include <iostream>
+#include <vector>
+
+void heapify(std::vector<long long>& heap, int size, long long currentRoot)
+{
+    long long largest = currentRoot;
+    long long left = 2 * currentRoot + 1;
+    long long right = 2 * currentRoot + 2;
+
+    // тут просто проверяю какой из детей больше
+    if (left < size && heap[left] > heap[largest])
+        largest = left;
+    if (right < size && heap[right] > heap[largest])
+        largest = right;
+
+    if (largest != currentRoot) // если нашли больший элемент чем наш то надо опять бахнуть кучу и проверить ее
+    {
+        std::swap(heap[currentRoot], heap[largest]);
+        heapify(heap, size, largest);
+    }
+}
+
+
+void deleteRoot(std::vector<long long>& arr, int& n)
+{
+    long long lastEl = arr[n - 1];
+    arr[0] = lastEl; // удаляю максимальный эл (корень главный) просто ставя его на последнее место
+    --n; // и делаю массив меньше
+    heapify(arr, n, 0);
+}
 
 int main()
 {
+    std::string operation;
+    std::vector<long long> vec;
+    int sizeOfVec = 0;
 
+    while (std::cin >> operation)
+    {
+        if (operation == "ADD")
+        {
+            long long num;
+            std::cin >> num;
+            vec.push_back(num);
+            int index = sizeOfVec;
+            ++sizeOfVec;
+            // проверяю если родитель меньше тек элемента то меняем их (ну по сути строю кучу)
+            while (index > 0 && vec[index] > vec[(index - 1) / 2])
+            {
+                std::swap(vec[index], vec[(index - 1) / 2]);
+                index = (index - 1) / 2;
+            }
+        }
+        else if (operation == "EXTRACT")
+        {
+            if (sizeOfVec > 0)
+            {
+                std::cout << vec[0] << '\n';
+                deleteRoot(vec, sizeOfVec);
+            }
+            else
+                std::cout << "CANNOT" << '\n';
+        }
+        else if (operation == "CLEAR")
+        {
+            vec.clear();
+            sizeOfVec = 0;
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -7,53 +7,67 @@ void heapify(std::vector<int>& heap, int size, int currentRoot)
     int left = 2 * currentRoot + 1;
     int right = 2 * currentRoot + 2;
 
-    // Проверьте, что left и right находятся в пределах size, а не currentRoot
+    // тут просто проверяю какой из детей больше
     if (left < size && heap[left] > heap[largest])
         largest = left;
     if (right < size && heap[right] > heap[largest])
         largest = right;
 
-    if (largest != currentRoot)
+    if (largest != currentRoot) // если нашли больший элемент чем наш, то надо опять бахнуть кучу и проверить ее
     {
         std::swap(heap[currentRoot], heap[largest]);
         heapify(heap, size, largest);
     }
 }
 
-void heapSort(std::vector<int>& array)
+void insert(std::vector<int>& arr, long long num)
 {
-    int size = array.size();
-
-    // Постройте начальную кучу
-    for (int i = size / 2 - 1; i >= 0; --i)
-        heapify(array, size, i);
-
-    // Постепенно уменьшайте размер кучи и перемещайте наибольший элемент в конец
-    for (int i = size - 1; i > 0; --i)
+    arr.push_back(num);
+    int index = arr.size() - 1;
+    // проверяю если родитель меньше тек элемента, то меняем их (ну по сути строю кучу)
+    while (index > 0 && arr[index] > arr[(index - 1) / 2])
     {
-        std::swap(array[0], array[i]);
-        heapify(array, i, 0);
+        std::swap(arr[index], arr[(index - 1) / 2]);
+        index = (index - 1) / 2;
     }
+}
+
+void deleteRoot(std::vector<int>& arr)
+{
+    int size = arr.size();
+    if (size <= 1) {
+        arr.pop_back();
+        return;
+    }
+    arr[0] = arr[size - 1];
+    arr.pop_back();
+    heapify(arr, arr.size(), 0);
 }
 
 int main()
 {
-    int sizeOfArray;
-    std::cin >> sizeOfArray;
-    std::vector<int> arr(sizeOfArray);
-    int el;
+    std::string operation;
+    std::vector<int> vec;
 
-    // Чтение элементов массива
-    for (int i = 0; i < sizeOfArray; ++i) {
-        std::cin >> el;
-        arr[i] = el;
+    while (std::cin >> operation)
+    {
+        if (operation == "ADD")
+        {
+            int num;
+            std::cin >> num;
+            insert(vec, num);
+        }
+        else if (operation == "EXTRACT")
+        {
+            if (!vec.empty())
+            {
+                std::cout << vec[0] << '\n';
+                deleteRoot(vec);
+            }
+            else
+                std::cout << "CANNOT" << '\n';
+        }
+        else if (operation == "CLEAR")
+            vec.clear();
     }
-
-    heapSort(arr);
-
-    // Вывод отсортированного массива
-    for (int el: arr)
-        std::cout << el << " ";
-
-    return 0;
 }
