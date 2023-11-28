98f5d6e5185ca24a58a3c52163a8e6ab8f2686d2
kaif777 <73842611+kaif777@users.noreply.github.com>
2023-11-29 02:52:26 +0300

    task1 contest 3

---
+++ contests/contest3/task1A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+void printPrefixFunction(const std::vector<int16_t>& preffArr)
+{
+    for (const int16_t& val : preffArr)
+        std::cout << val << " ";
+}
+
+void calculatePrefixFunction(const std::string& text)
+{
+    std::vector<int16_t> prefArr(text.size(), 0);
+    for (size_t i = 1; i < text.length(); ++i)
+    {
+        // initialize len to the value of the prefix function at the previous position
+        size_t lenOfLongestPref = prefArr[i - 1];
+        // backtrack if mismatch btw curChar and char at text[len],
+        // backtrack until 0 or match
+        while (lenOfLongestPref > 0 && text[i] != text[lenOfLongestPref])
+            lenOfLongestPref = prefArr[lenOfLongestPref - 1];
+
+        // match case
+        if (text[i] == text[lenOfLongestPref])
+            lenOfLongestPref++;
+
+        // update the prefix function value at the current position to lenOfLong
+        prefArr[i] = lenOfLongestPref;
+    }
+    printPrefixFunction(prefArr);
+}
+
+int main()
+{
+    std::string text;
+    std::cin >> text;
+    calculatePrefixFunction(text);
+}
