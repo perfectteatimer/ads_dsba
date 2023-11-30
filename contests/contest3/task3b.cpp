badf031154d427f6bbe1561bba579a2e2066cdf6
kaif777 <73842611+kaif777@users.noreply.github.com>
2023-11-30 14:52:11 +0300

    task2 contest 3

--- contests/contest3/task3b.cpp
+++ contests/contest3/task3b.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <string>
 
-bool areSubstrEqual(const std::string& s, int a, int b, int c, int d)
+// reference https://www.geeksforgeeks.org/string-hashing-using-polynomial-rolling-hash-function/
+
+bool hashPolynomial(const std::string& substr1, const std::string& substr2)
 {
-    if (b -  a != d - c) return false; // if sizes are not equal => false
-    else
+    long long p = 31;
+    long long m = 1e9 + 7;
+    long long hash1 = 0;
+    long long hash2 = 0;
+    long long p_pow = 1;
+    for (size_t i = 0; i < substr1.size(); ++i)
     {
-        for (size_t i = 0; i <= b - a; ++i) // compare characters for their entire length
-        {
-            if (s[a + i - 1] != s[c + i - 1]) return false; // comparing each charact of each substr
-        }
-        return true;
+        hash1 = ((hash1 + (substr1[i] - 'a' + 1) * p_pow) % m);
+        p_pow = (p_pow * p) % m;
+    }
+    p_pow = 1;
+    for (size_t i = 0; i < substr2.size(); ++i)
+    {
+        hash2 = ((hash2 + (substr2[i] - 'a' + 1) * p_pow) % m);
+        p_pow = (p_pow * p) % m;
     }
 
+    return hash1== hash2;
 }
 
-void compareSubstring(std::string &s, size_t m)
-{
+void compareSubstring(std::string& s, size_t m) {
     for (size_t i = 0; i < m; ++i)
     {
         int a, b, c, d;
         std::cin >> a >> b >> c >> d;
-        std::cout << (areSubstrEqual(s, a, b, c, d) ? "Yes": "No") << "\n";
+        std::string substr1 = s.substr(a - 1, b - a + 1);
+        std::string substr2 = s.substr(c - 1, d - c + 1);
+        std::cout << (hashPolynomial(substr1, substr2) ? "Yes" : "No") << '\n';
     }
 }
 
+
 int main()
 {
+    std::ios_base::sync_with_stdio(false); //ускоряет код
+    std::cin.tie(nullptr);
     std::string s;
     std::cin >> s;
     size_t m;
--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
-#include <map>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <boost>
 
+typedef long long ll;
+const ll P = 31;
+const ll M = 1e9 + 7;
 
+// Function to compute the hash of a string
+ll computeHash(const std::string& s) {
+    ll hash_value = 0;
+    ll p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % M;
+        p_pow = (p_pow * P) % M;
+    }
+    return hash_value;
+}
+
+void compareSubstring(const std::string& s, size_t m) {
+    // An unordered_map to store the precomputed hashes of substrings
+    std::unordered_map<std::pair<int, int>, ll, boost::hash<std::pair<int, int>>> hash_cache;
+
+    for (size_t i = 0; i < m; ++i) {
+        int a, b, c, d;
+        std::cin >> a >> b >> c >> d;
+        a--; b--; c--; d--; // Convert to zero-based indexing
+
+        // Check if the hashes are already computed
+        std::pair<int, int> substr1_indices = {a, b};
+        std::pair<int, int> substr2_indices = {c, d};
+
+        if (hash_cache.find(substr1_indices) == hash_cache.end()) {
+            hash_cache[substr1_indices] = computeHash(s.substr(a, b - a + 1));
+        }
+        if (hash_cache.find(substr2_indices) == hash_cache.end()) {
+            hash_cache[substr2_indices] = computeHash(s.substr(c, d - c + 1));
+        }
+
+        std::cout << (hash_cache[substr1_indices] == hash_cache[substr2_indices] ? "Yes" : "No") << '\n';
+    }
+}
 
 int main() {
-    std::cout << "dd";
-    return 0;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string s;
+    std::cin >> s;
+    size_t m;
+    std::cin >> m;
+    compareSubstring(s, m);
 }
