36135b8f9ce6b563fbf745ca71a2740b53eacc67
roman <romanbokhyan@gmail.com>
2023-12-17 01:50:25 +0300

    contest 4

--- contests/contest4/task4A.cpp
+++ contests/contest4/task4A.cpp
@@ -1,3 +1,20 @@
-//
-// Created by 1 on 12/17/2023.
-//
+#include <iostream>
+
+int64_t numberOfWays(int64_t numberOfSteps)
+{
+    if (numberOfSteps == 1 or numberOfSteps == 2)
+        return numberOfSteps;
+    if (numberOfSteps == 3)
+        return 4;
+    else
+        return (numberOfWays(numberOfSteps - 1) +
+        numberOfWays(numberOfSteps - 2) +
+        numberOfWays(numberOfSteps - 3));
+}
+
+int main()
+{
+    int64_t numberOfSteps;
+    std::cin >> numberOfSteps;
+    std::cout<<numberOfWays(numberOfSteps);
+}
