c0a48286f0465fd75276a2f9b2f2e8c508a72873
roman <romanbokhyan@gmail.com>
2024-01-13 00:28:54 +0300

    check

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 
-int countSafeStacks(int n)
-{
-    std::pair<int, int> last = {1, 1}; //  for n = 1
-    std::pair<int, int> current;
-    for (int i = 2; i <= n; ++i)
-    {
-        current.first = last.first + last.second; //ending with 'B'
-        current.second = last.first; //  ending with 'A'
-        last = current; //for the next iteration
-    }
-    return current.first + current.second; // total safe
-}
-
 int main()
 {
     int n;
     std::cin >> n;
-    std::cout << countSafeStacks(n);
 }
