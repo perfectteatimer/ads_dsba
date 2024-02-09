25117d25ce413c24ce608bbeaef3b580502bf2fc
kaif777 <73842611+kaif777@users.noreply.github.com>
2024-02-10 01:00:47 +0300

    graph

---
+++ contests/contest5/task5A.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <queue>
+#include <stack>
+// code from seminar
+using Graph = std::unordered_map<int, std::unordered_set<int>>;
+
+// bfs algorithm 
+void traverse(Graph& g, int start)
+{
+    std::vector<bool> visited(g.size(), false);
+    std::queue<int> q; // change queue to stack for dfs
+    q.push(start);
+    visited[start] = true;
+    int count = 0;
+    while(!q.empty())
+    {
+        int x = q.front();
+        q.pop();
+        ++count;
+        for (auto& i : g[x])
+        {
+            if (!visited[i])
+            {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+
+    }
+    std::cout << count;
+}
+
+int main()
+{
+    int n, start;
+    std::cin >> n >> start;
+    Graph graph(n);
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            int edge;
+            std::cin >> edge;
+            if(edge == 1)
+            {
+                graph[i].insert(j);
+                graph[j].insert(i); // Since the graph is undirected
+            }
+        }
+    }
+
+    traverse(graph, start-1);
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,11 @@
 #include <vector>
 #include <iostream>
 
-void countVisitedCells(std::vector<std::vector<int>>& room, int n, int m)
-{
-    char ch;
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < m; ++j)
-        {
-            std::cin >> ch;
-            if (ch == '#')
-                room[i][j] = 1;
-        }
-    }
-    int q, counter = 0;
-    std::cin >> q;
-    std::string str;
-    std::cin >> str;
-    for (size_t i = 0; i < q; ++i)
-    {
-        if (str[i] == )
-    }
-
-}
 
 int main()
 {
     int n, m;
-    std::cin >> n >> m;
-    std::vector<std::vector<int>> room(n, std::vector<int>(m, 0));
-    countVisitedCells(room, n, m);
+    std::cin>>n>>m;
+    std::cout<<((n == (m + 3)) ? 0 : 1);
 }
 
