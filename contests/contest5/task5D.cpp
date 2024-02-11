b4ff516473a3d98358bba94aad318d2db4149178
kaif777 <73842611+kaif777@users.noreply.github.com>
2024-02-12 00:00:44 +0300

    contest 5

--- contests/contest5/task5D.cpp
+++ contests/contest5/task5D.cpp
@@ -1,3 +1,71 @@
-//
-// Created by 1 on 2/9/2024.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// using интернет много и семинар
+
+using Graph = std::vector<std::vector<int>>;
+
+bool storeCyclesIfExist(const Graph& g, int v, std::vector<int>& parent, std::vector<bool>& visited, std::vector<bool>& recStack, std::vector<int>& cycle)
+{
+    visited[v] = true;
+    recStack[v] = true;
+
+    for (int u: g[v])
+    {
+        if (!visited[u])
+        {
+            parent[u] = v; // сохраняем предка для восстановления пути
+            if (storeCyclesIfExist(g, u, parent, visited, recStack, cycle)) return true;
+        }
+        else if (recStack[u])
+        {
+            // восстанавливаем цикл
+            int cur = v;
+            cycle.push_back(cur);
+            while (cur != u)
+            {
+                cur = parent[cur];
+                cycle.push_back(cur);
+            }
+            return true;
+        }
+    }
+
+    recStack[v] = false;
+    return false;
+}
+
+int main()
+{
+    int N, M;
+    std::cin >> N >> M;
+    Graph graph(N);
+    std::vector<int> parent(N, -1);
+    std::vector<bool> visited(N, false), recStack(N, false);
+    std::vector<int> cycle;
+
+    for (int i = 0; i < M; ++i)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        a--;
+        b--;
+        graph[a].push_back(b);
+    }
+
+    bool cycleFound = false;
+    for (int i = 0; i < N && !cycleFound; ++i)
+    {
+        if (!visited[i] && storeCyclesIfExist(graph, i, parent, visited, recStack, cycle))
+        {
+            cycleFound = true;
+            std::cout << "YES\n";
+            for (auto it = cycle.rbegin(); it != cycle.rend(); ++it)
+                std::cout << *it + 1 << " ";
+            std::cout << "\n";
+        }
+    }
+
+    if (!cycleFound)
+        std::cout << "NO";
+}
--- contests/contest5/task5F.cpp
+++ contests/contest5/task5F.cpp
@@ -1,3 +1,50 @@
-//
-// Created by 1 on 2/9/2024.
-//
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+ // using интернет и семинар
+
+ // идея просто сделать дфс от начальной вершины, найти самую дальнюю вершину, и от нее сделать еще один дфс и найти максимальное расстояние
+using Graph = std::unordered_map<int, std::vector<int>>;
+
+
+void dfs(const Graph& g, int v, std::vector<bool>& visited, int& furthestNode, int& maxLength, int length = 0)
+{
+    visited[v] = true;
+    if(length > maxLength)
+    {
+        maxLength = length;
+        furthestNode = v;
+    }
+    for(auto& u : g.at(v))
+    {
+        if(!visited[u])
+            dfs(g, u, visited, furthestNode, maxLength, length + 1);
+
+    }
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    Graph graph;
+    for(int i = 0; i < n - 1; ++i)
+    {
+        int u, v;
+        std::cin >> u >> v;
+        --u; --v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    int start = 0;
+    std::vector<bool> visited(n, false);
+    int furthestNode = 0;
+    int maxLength = 0;
+    // первый DFS для нахождения самой дальней вершины от начальной точки
+    dfs(graph, start, visited, furthestNode, maxLength);
+    std::fill(visited.begin(), visited.end(), false);
+    maxLength = 0;
+    // второй DFS для определения максимального расстояния в графе
+    dfs(graph, furthestNode, visited, furthestNode, maxLength);
+    std::cout << maxLength + 1;
+
+}
