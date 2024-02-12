2ca3d9cb5647fdd2516039544f1c7dfd1133f03e
roman <romanbokhyan@gmail.com>
2024-02-12 22:28:36 +0300

    main

---
+++ definers .h
@@ -0,0 +1,14 @@
+//
+// Created by roman bokhyan on 12.02.2024.
+//
+
+#ifndef ALGORITHMSDSBA__DEFINERS_H
+#define ALGORITHMSDSBA__DEFINERS_H
+
+#endif //ALGORITHMSDSBA__DEFINERS_H
+#define pb push_back
+#define input std::cin >>
+#define output std::cout <<
+#define endl std::endl
+#define space " "
+#define fori(n) for(int i = 0; i < n; ++i)
